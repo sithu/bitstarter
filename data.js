40e03b69590bb2bbc32e4a55632e9bd61a24a1bd
sithu <sithumail@gmail.com>
2013-09-13 10:52:31 -0700

    added final steps of trademark recipe

--- data.js
+++ data.js
@@ -6,14 +6,25 @@ var recipes = [
 	id: 1,
 	steps: [
 		{ step: 1, 
-			desc: "Start with search engines -- Once you have brainstormed a name you like, start with a basic Google or Bing search to see what else may already be out there with a similar name.  This can help you uncover obvious potential conflicts that may cause you to pursue a different name.", 
+			desc: "Start with search engines - Once you have brainstormed a name you like, start with a basic Google or Bing search to see what else may already be out there with a similar name.  This can help you uncover obvious potential conflicts that may cause you to pursue a different name.", 
 			time: "20 min" },
 		{ step: 2, 
 			desc: "Explore domain names – Using a safe, known domain registrar’s look up tool, explore whether there is already a similar business in your country who already controls the '.com' or other domain names.  This, again, can help you determine whether your chosen name is already owned as a trademark by someone else.", 
 			time: "15 min" },
 		{ step: 3, 
 			desc: "Explore your state’s LLC or other corporate name registry – Most states have an easily searchable database of corporate/LLC names.  This can give you a sense for whether there are other companies in your state or region who may already have adopted a similar name.", 
-			time: "20 min", divider: true }
+			time: "20 min", divider: true },
+		{
+			step: 4,
+			desc: "Initially explore the U.S. Trademark Registry – While the U.S. Trademark Registry search process can be extremely complex and often requires deep legal expertise to produce and analyze meaningful results, you can do some initial searching – for example to uncover exact matches for identical marks that may already exist in the same marketplace in which you plan to do business.  At minimum, this can help you determine if the mark you want to use is already a registered trademark of another business that provides products or services that are competitive or somehow related to your own – or already established as a famous mark of a large company.",
+			time: "it depends"
+		},
+		{
+			step: 5,
+			desc: "Obtain the right legal advice - If the name will be an important/critical element of your business strategy, you may owe it to yourself to identify and invest in a trusted legal advisor who specializes in trademark clearance.  An experienced trademark lawyer can ensure that you obtain a meaningful and extensive trademark search report, and then help you interpret that report and decide if the name you want to adopt is legally available and defensible.  Finding the right lawyer often involves referrals from trusted friends or colleagues, or a referral from a reputable lawyer referral service or local bar association.",
+			time: "it depends", 
+			divider: true
+		}
 	],
 	total_time: "55 min",
 	total_cost: "Free"
